4c7371f80af4cc6233adec2f95c0737ec4f51d5f
CrisSeg <cristianseg0122@gmail.com>
2024-06-03 16:45:28 -0300

    Menu con modifcaciones y las funciones de agregar jefe de obra y mostrar la lista de obreros hecha

--- Program.cs
+++ Program.cs
@@ -1,123 +1,177 @@
-using System;
+﻿using System;
 using System.Collections;
 
-namespace Practica_5
+namespace Proyecto
 {
 	class Program
 	{
-		static ArrayList listaAlumnos = new ArrayList();
-		static ArrayList ListaMaterias = new ArrayList();
-		
 		public static void Main(string[] args)
 		{
-			int opcion;
-			Console.WriteLine("##### MENU #####");
-			Console.WriteLine("1_ Inscribir alumno");
-			Console.WriteLine("2_ Agregar materia.");
-			Console.WriteLine("3_ -incribirse a una materia");
-			Console.WriteLine("4_ Materias que cursa alumno");
-			Console.WriteLine("6_ salir");
-			Console.WriteLine("Elija una opcion: ");
-			opcion = int.Parse(Console.ReadLine());
-			while (opcion != 6) {
-				switch (opcion) {
-					case 1:
-						InscribirAlumno();
-						break;
-					case 2:
-						AgregarMateria();
-						break;
-					case 3:
-						InscribirMateria();
-						break;
-					case 4: 
-						MateriasAlumno();
-						break;
-					default:
-						Console.WriteLine("Opcion incorrecta.");
-						Console.WriteLine("Intente nuevamente.");
-						break;
-				}
-				
-				Console.WriteLine("##### MENU #####");
-				Console.WriteLine("1_ Inscribir alumno");
-				Console.WriteLine("2_ Agregar materia.");
-				Console.WriteLine("3_ incribirse a una materia");
-				Console.WriteLine("4_ Materias que cursa alumno");
-				Console.WriteLine("6_ salir");
-				Console.WriteLine("Elija una opcion: ");
-				opcion = int.Parse(Console.ReadLine());
+			Empresa emp = new Empresa(); //Creo la empresa donde vamos a realizar el proyecto
+			try {
+				Console.WriteLine("Menú de opciones:");
+           	 	Console.WriteLine("1. Contratar un nuevo obrero");
+            	Console.WriteLine("2. Eliminar un obrero");
+            	Console.WriteLine("3. Contratar a un jefe de obra");
+		    	Console.WriteLine("4. Modificar el estado de avance de una obra.");
+	    		Console.WriteLine("5. Ver listados y procentajes");
+	    		Console.WriteLine("6. Dar de baja a un jefe");
+            	Console.WriteLine("7. Salir");
+            	Console.Write("Seleccione una opción: ");
+            	string opcion = Console.ReadLine();
+            	
+            	while (opcion != "7") {
+            		switch (opcion){
+            			
+            				case "1":
+                    			AgregarObreros(emp); //Paso la empresa que cree, para cada una de las funciones 
+                    			break;
+                			case "2":
+                    			EliminarObrero(emp);
+                    			break;
+                			case "3":
+                    			AgregarJefeDeOBra(emp);
+                    			break;
+                			case "4":
+                    			ModificarEstadoObra();
+                    			break;
+                    		case "5":
+		    		 			Console.WriteLine("Submenú de opciones:");
+		     		 			Console.WriteLine("1. Mostrar listado de obreros");
+		     		 			Console.WriteLine("2. Mostrar listado de obras en ejecución");
+		     		 			Console.WriteLine("3. Mostrar listado de obras finalizadas");
+		     		 			Console.WriteLine("4. Mostrar listado de jefes");
+		     		 			Console.WriteLine("5. Mostrar porcentaje de obras de remodelación sin finalizar");
+		     		 			Console.WriteLine("6. Salir");
+		             			Console.Write("Seleccione una opción: ");
+		     		 			string op = Console.ReadLine();
+		     		 			while (op != "6") {
+		     		 				switch(op){
+                            			case "1":
+		     		 						ListaDeObreros(emp);
+                             				break;
+			    						case "2":
+                    	         			ListaObrasEj();
+                    	         			break;
+			    						case "3":
+                            	 			ListaObrasFin();
+                            	 			break;
+			    						case "4":
+                            	 			ListaObreros();
+                            	 			break;
+			    						case "5":
+                            	 			ListaObrasEj();
+                            	 			break;
+                            			default:
+                            	 			Console.WriteLine("Opción no válida.");
+                            	 			break;
+                    	            }
+		     		 			}
+		     		 			break;
+		     		 		case "6":
+                    			EliminarJefeObra();
+                    			break;		
+                			default:
+                    			Console.WriteLine("Opción no válida.");
+                    			break;		
+            		}
+            	}
+			  }
+            catch (PuestoOcupado) {
+				Console.WriteLine("El jefe de obra no se pudo asignar a la obra o grupo");}
+			catch(Exception){
 			}
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
+		static void AgregarObreros(Empresa emp){  
+        	Console.Write("Ingrese nombre completo: ");
+        	string nombreObrero = Console.ReadLine();
+        	Console.Write("Ingrese dni: ");
+        	int dni = int.Parse(Console.ReadLine());
+        	Console.Write("Ingrese número de legajo: ");
+        	int nroLegajo = int.Parse(Console.ReadLine());
+        	Console.Write("Ingrese el sueldo: ");
+        	double sueldo =  double.Parse(Console.ReadLine());
+			Console.Write("Ingrese su cargo: ");
+			string cargo = Console.ReadLine();
 		
-		static void InscribirAlumno()
-		{
-			string nombre;
-			int dni,legajo;
-		    	
-			Console.WriteLine("Ingrese el nombre del alumno: ");
-			nombre = Console.ReadLine();
-			Console.WriteLine("Ingrese el dni del alumno: ");
-			dni = int.Parse(Console.ReadLine());
-			Console.WriteLine("Ingrese el legajo del alumno: ");
-			legajo = int.Parse(Console.ReadLine());
-			
-			Alumno a = new Alumno(nombre, dni, legajo);
-			listaAlumnos.Add(a);
-			Console.WriteLine("El alumno se inscribio con existo.");
-		}
 		
-		static void AgregarMateria()
-		{
-			Console.WriteLine("Ingrese la materia: ");
-			string materia = Console.ReadLine();
-			Console.WriteLine("Ingrese el dia: ");
-			string dia = Console.ReadLine();
-			Console.WriteLine("Ingrese el horario: ");
-			string horario = Console.ReadLine();
-			
-			Horario h = new Horario(materia,dia,horario);
-			ListaMaterias.Add(h);
-			Console.WriteLine("La materia se agrego con existo.");
-		}
+        	Obrero nuevoObrero = new Obrero(nombreObrero, dni, nroLegajo, sueldo, cargo);
+        	emp.AgregarObrero(nuevoObrero);
+        	Console.WriteLine("Obrero agregado correctamente.");
+    	}
 		
-		static void InscribirMateria()
+		static void EliminarObrero(Empresa emp)
+        {
+    		Console.Write("Ingrese el número de legajo del obrero a eliminar : ");
+    		int nroLegajo = int.Parse(Console.ReadLine());
+
+    		ArrayList listObreros = new ArrayList();
+    		listObreros = emp.TodosObreros();
+        	foreach(Obrero o in listObreros)
+        	{
+        		if(o.NroLegajo == nroLegajo)
+        		{
+        			emp.ELiminarObrero(o);
+        			Console.WriteLine("El obrero se elimino con exito.");
+        		}
+        		else{
+        			Console.WriteLine("El obrero no se elimino con exito.");
+        			Console.WriteLine("El obrero es inexistente o ingrese el número de legajo correctamente.");
+        		}
+        	}
+    	}
+		
+		static void AgregarJefeDeOBra(Empresa emp)
 		{
-			int dniAl;
-			string mat, dia, hor;
+			Console.Write("Ingrese nombre completo: ");
+        	string nombreObrero = Console.ReadLine();
+        	Console.Write("Ingrese dni: ");
+        	int dni = int.Parse(Console.ReadLine());
+        	Console.Write("Ingrese número de legajo: ");
+        	int nroLegajo = int.Parse(Console.ReadLine());
+        	Console.Write("Ingrese el sueldo: ");
+        	double sueldo =  double.Parse(Console.ReadLine());
+			Console.Write("Ingrese su cargo: ");
+			string cargo = Console.ReadLine();
+			Console.WriteLine("Asigne número de obra:");
+			int numObra = int.Parse(Console.ReadLine());
+			Console.WriteLine("Asigne un grupo libre(número de grupo): ");
+			int numGrupo = int.Parse(Console.ReadLine());
+			Console.WriteLine("Ingrese la bonficacion que se la va a dar al jefe: ");
+			double bonificacion = double.Parse(Console.ReadLine());
 			
-			Console.WriteLine("ingrese el dni del alumno: ");
-			dniAl = int.Parse(Console.ReadLine());
-			foreach (Alumno elem in listaAlumnos) {               //Recorro la lista de alumnos
-				if (dniAl == elem.Dni) {                          //comparo el dni pedido con los de la lista
-					Console.WriteLine("Nombre de la materia: ");  //si el dni esta en la lista pido los atributos de la materia
-					mat = Console.ReadLine();                     //para agregar a la lista de horarios de alumno   
-					Console.WriteLine("Dia de curasda: ");
-					dia = Console.ReadLine();
-					Console.WriteLine("Horario de cursada: ");
-					hor = Console.ReadLine();
-					
-					elem.InscibirserMateria(mat, dia, hor);
+			JefeObra jfObra = new JefeObra(nombreObrero, dni, nroLegajo, sueldo, cargo, numObra, numGrupo, bonificacion);
+			ArrayList listObras = new ArrayList();
+			listObras = emp.TodasObras();
+			ArrayList listGrupos = new ArrayList();
+			listGrupos = emp.TodosGrupos();
+			foreach (Obra elem in listObras) { //Recorro la lista de obras de la empresa
+				foreach (Grupo ele in listGrupos) {  //Recorro la lista de grupos de la empresa
+					if (elem.CodigoObra == numObra && ele.NroObra == numObra) {  //Comparo el codigo de la obra y grupo con los que pido por teclado
+						if (elem.DniJefObra != 0 && ele.DniJefObra != 0) {  //Una vez que encuentre la obra y grupo que pido por teclado 
+							throw new PuestoOcupado();                      //Veo si el puesto esta ocupado tanto en la obra como el grupo 
+						}                                                   //Si esta ocupado dispara el throw de la excepción  
+						else{                                               //Sino agrego el jefe de obra a la lista de obreros y modifico el dniJefeObra en la obra y el grupo    
+							emp.AgregarObrero(jfObra);
+							elem.DniJefObra = dni;
+							ele.DniJefObra = dni;
+						}
+					}
 				}
-				else
-					Console.WriteLine("El alumno no esta inscripto."); 
 			}
 		}
+		public class PuestoOcupado: Exception
+		{}
 		
-		static void MateriasAlumno()
+		static void ListaDeObreros(Empresa emp)
 		{
-			int dniAlu;
-			
-			Console.WriteLine("ingrese el dni del alumno: ");
-			dniAlu = int.Parse(Console.ReadLine());
-			
-			foreach (Alumno elem in listaAlumnos) {
-				if (elem.Dni == dniAlu) {
-					Console.WriteLine("El alumno esta inscripto a {0}", elem.CantidadMaterias());
-				}
+			ArrayList listaDeObreros = new ArrayList();
+			listaDeObreros = emp.TodosObreros();
+			foreach (Obrero elem in listaDeObreros) {
+				Console.WriteLine(elem);
 			}
 		}
 	}
