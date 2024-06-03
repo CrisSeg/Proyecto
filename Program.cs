5e916e344a1754fabc6ed4c4874ddae1b2fb4c77
CrisSeg <cristianseg0122@gmail.com>
2024-06-03 16:52:54 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,17 +11,17 @@ namespace Proyecto
 			try {
 				Console.WriteLine("Menú de opciones:");
            	 	Console.WriteLine("1. Contratar un nuevo obrero");
-            	Console.WriteLine("2. Eliminar un obrero");
-            	Console.WriteLine("3. Contratar a un jefe de obra");
+            	      	Console.WriteLine("2. Eliminar un obrero");
+            		Console.WriteLine("3. Contratar a un jefe de obra");
 		    	Console.WriteLine("4. Modificar el estado de avance de una obra.");
 	    		Console.WriteLine("5. Ver listados y procentajes");
 	    		Console.WriteLine("6. Dar de baja a un jefe");
-            	Console.WriteLine("7. Salir");
-            	Console.Write("Seleccione una opción: ");
-            	string opcion = Console.ReadLine();
+            		Console.WriteLine("7. Salir");
+            		Console.Write("Seleccione una opción: ");
+            		string opcion = Console.ReadLine();
             	
-            	while (opcion != "7") {
-            		switch (opcion){
+            		while (opcion != "7") {
+            			switch (opcion){
             			
             				case "1":
                     			AgregarObreros(emp); //Paso la empresa que cree, para cada una de las funciones 
@@ -35,57 +35,56 @@ namespace Proyecto
                 			case "4":
                     			ModificarEstadoObra();
                     			break;
-                    		case "5":
-		    		 			Console.WriteLine("Submenú de opciones:");
-		     		 			Console.WriteLine("1. Mostrar listado de obreros");
-		     		 			Console.WriteLine("2. Mostrar listado de obras en ejecución");
-		     		 			Console.WriteLine("3. Mostrar listado de obras finalizadas");
-		     		 			Console.WriteLine("4. Mostrar listado de jefes");
-		     		 			Console.WriteLine("5. Mostrar porcentaje de obras de remodelación sin finalizar");
-		     		 			Console.WriteLine("6. Salir");
+                    			case "5":
+		    		 		Console.WriteLine("Submenú de opciones:");
+		     		 		Console.WriteLine("1. Mostrar listado de obreros");
+		     		 		Console.WriteLine("2. Mostrar listado de obras en ejecución");
+		     		 		Console.WriteLine("3. Mostrar listado de obras finalizadas");
+		     		 		Console.WriteLine("4. Mostrar listado de jefes");
+		     		 		Console.WriteLine("5. Mostrar porcentaje de obras de remodelación sin finalizar");
+		     		 		Console.WriteLine("6. Salir");
 		             			Console.Write("Seleccione una opción: ");
-		     		 			string op = Console.ReadLine();
-		     		 			while (op != "6") {
-		     		 				switch(op){
-                            			case "1":
-		     		 						ListaDeObreros(emp);
-                             				break;
-			    						case "2":
-                    	         			ListaObrasEj();
-                    	         			break;
-			    						case "3":
-                            	 			ListaObrasFin();
-                            	 			break;
-			    						case "4":
-                            	 			ListaObreros();
-                            	 			break;
-			    						case "5":
-                            	 			ListaObrasEj();
-                            	 			break;
-                            			default:
-                            	 			Console.WriteLine("Opción no válida.");
-                            	 			break;
-                    	            }
-		     		 			}
-		     		 			break;
-		     		 		case "6":
-                    			EliminarJefeObra();
-                    			break;		
+		     		 		string op = Console.ReadLine();
+		     		 		while (op != "6") {
+		     		 			switch(op){
+                            					case "1":
+		     		 					ListaDeObreros(emp);
+                             						break;
+			    					case "2":
+                    	         					ListaObrasEj();
+                    	         					break;
+			    					case "3":
+                            	 					ListaObrasFin();
+                            	 					break;
+			    					case "4":
+                            	 					ListaObreros();
+                            	 					break;
+			    					case "5":
+                            	 					ListaObrasEj();
+                            	 					break;
+                            					default:
+                            	 					Console.WriteLine("Opción no válida.");
+                            	 					break;
+                    	                                 }
+		     		 		}
+		     		 		break;
+		     		 	case "6":
+                    				EliminarJefeObra();
+                    				break;		
                 			default:
-                    			Console.WriteLine("Opción no válida.");
-                    			break;		
+                    				Console.WriteLine("Opción no válida.");
+                    				break;		
             		}
-            	}
-			  }
-            catch (PuestoOcupado) {
+            	     }
+	        }
+            		catch (PuestoOcupado) {
 				Console.WriteLine("El jefe de obra no se pudo asignar a la obra o grupo");}
-			catch(Exception){
+	    		catch(Exception){
 			}
-			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
-		static void AgregarObreros(Empresa emp){  
+	static void AgregarObreros(Empresa emp){  
         	Console.Write("Ingrese nombre completo: ");
         	string nombreObrero = Console.ReadLine();
         	Console.Write("Ingrese dni: ");
@@ -103,8 +102,7 @@ namespace Proyecto
         	Console.WriteLine("Obrero agregado correctamente.");
     	}
 		
-		static void EliminarObrero(Empresa emp)
-        {
+	static void EliminarObrero(Empresa emp){
     		Console.Write("Ingrese el número de legajo del obrero a eliminar : ");
     		int nroLegajo = int.Parse(Console.ReadLine());
 
@@ -124,9 +122,8 @@ namespace Proyecto
         	}
     	}
 		
-		static void AgregarJefeDeOBra(Empresa emp)
-		{
-			Console.Write("Ingrese nombre completo: ");
+	static void AgregarJefeDeOBra(Empresa emp){
+		Console.Write("Ingrese nombre completo: ");
         	string nombreObrero = Console.ReadLine();
         	Console.Write("Ingrese dni: ");
         	int dni = int.Parse(Console.ReadLine());
@@ -134,31 +131,30 @@ namespace Proyecto
         	int nroLegajo = int.Parse(Console.ReadLine());
         	Console.Write("Ingrese el sueldo: ");
         	double sueldo =  double.Parse(Console.ReadLine());
-			Console.Write("Ingrese su cargo: ");
-			string cargo = Console.ReadLine();
-			Console.WriteLine("Asigne número de obra:");
-			int numObra = int.Parse(Console.ReadLine());
-			Console.WriteLine("Asigne un grupo libre(número de grupo): ");
-			int numGrupo = int.Parse(Console.ReadLine());
-			Console.WriteLine("Ingrese la bonficacion que se la va a dar al jefe: ");
-			double bonificacion = double.Parse(Console.ReadLine());
+		Console.Write("Ingrese su cargo: ");
+		string cargo = Console.ReadLine();
+		Console.WriteLine("Asigne número de obra:");
+		int numObra = int.Parse(Console.ReadLine());
+		Console.WriteLine("Asigne un grupo libre(número de grupo): ");
+		int numGrupo = int.Parse(Console.ReadLine());
+		Console.WriteLine("Ingrese la bonficacion que se la va a dar al jefe: ");
+		double bonificacion = double.Parse(Console.ReadLine());
 			
-			JefeObra jfObra = new JefeObra(nombreObrero, dni, nroLegajo, sueldo, cargo, numObra, numGrupo, bonificacion);
-			ArrayList listObras = new ArrayList();
-			listObras = emp.TodasObras();
-			ArrayList listGrupos = new ArrayList();
-			listGrupos = emp.TodosGrupos();
-			foreach (Obra elem in listObras) { //Recorro la lista de obras de la empresa
-				foreach (Grupo ele in listGrupos) {  //Recorro la lista de grupos de la empresa
-					if (elem.CodigoObra == numObra && ele.NroObra == numObra) {  //Comparo el codigo de la obra y grupo con los que pido por teclado
-						if (elem.DniJefObra != 0 && ele.DniJefObra != 0) {  //Una vez que encuentre la obra y grupo que pido por teclado 
-							throw new PuestoOcupado();                      //Veo si el puesto esta ocupado tanto en la obra como el grupo 
+		JefeObra jfObra = new JefeObra(nombreObrero, dni, nroLegajo, sueldo, cargo, numObra, numGrupo, bonificacion);
+		ArrayList listObras = new ArrayList();
+		listObras = emp.TodasObras();
+		ArrayList listGrupos = new ArrayList();
+		listGrupos = emp.TodosGrupos();
+		foreach (Obra elem in listObras) { //Recorro la lista de obras de la empresa
+			foreach (Grupo ele in listGrupos) {  //Recorro la lista de grupos de la empresa
+				if (elem.CodigoObra == numObra && ele.NroObra == numObra) {  //Comparo el codigo de la obra y grupo con los que pido por teclado
+					if (elem.DniJefObra != 0 && ele.DniJefObra != 0) {  //Una vez que encuentre la obra y grupo que pido por teclado 
+						throw new PuestoOcupado();                      //Veo si el puesto esta ocupado tanto en la obra como el grupo 
 						}                                                   //Si esta ocupado dispara el throw de la excepción  
 						else{                                               //Sino agrego el jefe de obra a la lista de obreros y modifico el dniJefeObra en la obra y el grupo    
-							emp.AgregarObrero(jfObra);
+						 	emp.AgregarObrero(jfObra);
 							elem.DniJefObra = dni;
-							ele.DniJefObra = dni;
-						}
+							ele.DniJefObra = dni;}
 					}
 				}
 			}
@@ -166,8 +162,7 @@ namespace Proyecto
 		public class PuestoOcupado: Exception
 		{}
 		
-		static void ListaDeObreros(Empresa emp)
-		{
+		static void ListaDeObreros(Empresa emp){
 			ArrayList listaDeObreros = new ArrayList();
 			listaDeObreros = emp.TodosObreros();
 			foreach (Obrero elem in listaDeObreros) {
